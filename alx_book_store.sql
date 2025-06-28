0b019d8d73a7eb0a63d576634b7679b71477a0a3
Kelvin Udufo <kelvyn@Kelvins-MacBook-Pro.local>
2025-06-28 15:28:55 +0100

    updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -43,4 +43,5 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     book_id INT,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOR
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
